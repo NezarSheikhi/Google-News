a47c55a238038300b37ccf460c23c35345e9a0ea
Nezar Sheikhi <nezar.sheikhi@icloud.com>
2024-02-08 21:24:00 +0100

    Update app.js
    source update

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ $(function(){
     // Get Api
     function getRequest(input){
         var url = 'https://newsapi.org/v2/top-headlines?' +
-                  'country='+input 
+                  'country='+input +
+                'category='+ business
         var rules = {
             apiKey: 'f72d71564c7f4912a4e8962f0dcac996',
             s: input,
